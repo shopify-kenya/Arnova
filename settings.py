39149763bd06fbdabdd6d0b29ec7972403deaf9d
Murzuqisah <amosjoel229@gmail.com>
2026-01-16 11:29:09 +0300

    fix(api): change REST Framework default permission to AllowAny
    Views handle their own authentication with decorators

--- settings.py
+++ settings.py
@@ -207,10 +207,9 @@ MPESA_CALLBACK_URL = os.getenv("MPESA_CALLBACK_URL")
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.AllowAny",
     ],
 }
 
